1261605c26dbcca6e8cbdd59baffffecaab2dbf7
parsafrd <hamid.jahan125z@gmail.com>
2024-01-15 16:11:10 +0330

    Update Objects.py
    defining_objects_step1

--- Objects.py
+++ Objects.py
@@ -1,6 +1,6 @@
 class User:
-    def __init__(self, user_id, name, email, password, user_type):
-        self.user_id = user_id
+    def __init__(self, id, name, email, password, user_type):
+        self.id = id
         self.name = name
         self.email = email
         self.password = password
@@ -9,88 +9,106 @@ class User:
     def register(self):
         pass
 
-    # Code for user registration
-
     def login(self):
         pass
-
-    # Code for user login
 
     def update_profile(self):
         pass
 
-    # Code for updating user profile
-
     def view_appointments(self):
         pass
 
 
-# Code for user to view their appointments
-
-
 class Clinic:
-    def __init__(self, clinic_id, name, address, contact_info, services, available):
-        self.clinic_id = clinic_id
+    def __init__(self, id, name, address, contact, services, availability):
+        self.id = id
         self.name = name
         self.address = address
-        self.contact_info = contact_info
+        self.contact = contact
         self.services = services
-        self.available = available
+        self.availability = availability
 
     def add_clinic(self):
         pass
 
-    # Code for adding a new clinic
-
     def update_clinic_info(self):
         pass
 
-    # Code for updating clinic information
-
     def set_availability(self):
         pass
-
-    # Code for setting clinic availability
 
     def view_appointments(self):
         pass
 
 
-# Code for clinic to view appointments
+class Doctor:
+    def __init__(self, id, name, clinic, specialization):
+        self.id = id
+        self.name = name
+        self.clinic = clinic
+        self.specialization = specialization
+
+    def add_doctor(self):
+        pass
+
+    def update_doctor_info(self):
+        pass
+
+    def view_appointments(self):
+        pass
 
 
 class Appointment:
-    def __init__(self, appointment_id, clinic_id, user_id, date_time, status):
-        self.appointment_id = appointment_id
+    def __init__(self, id, clinic_id, patient_id, date_time, status):
+        self.id = id
         self.clinic_id = clinic_id
-        self.user_id = user_id
+        self.patient_id = patient_id
         self.date_time = date_time
         self.status = status
 
     def book_appointment(self):
         pass
 
-    # Code for user to book an appointment
-
     def cancel_appointment(self):
         pass
-
-    # Code for user to cancel an appointment
 
     def reschedule_appointment(self):
         pass
 
 
-# Code for user to reschedule an appointment
-
-
 class Notification:
-    def __init__(self, notification_id, user_id, message, date_time):
-        self.notification_id = notification_id
+    def __init__(self, id, user_id, message, date_time):
+        self.id = id
         self.user_id = user_id
         self.message = message
         self.date_time = date_time
 
     def send_notification(self):
-        # Code for sending a notification to a user
         pass
+
+
+class Admin:
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def add_user(self):
+        pass
+
+    def add_clinic(self):
+        pass
+
+    def add_doctor(self):
+        pass
+
+
+class System:
+    def __init__(self):
+        self.users = []
+        self.clinics = []
+        self.doctors = []
+        self.appointments = []
+        self.notifications = []
+
